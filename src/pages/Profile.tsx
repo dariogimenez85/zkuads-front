68d2e01cb55938627a8990c2cd65d832ed94e7f0
Dario Gimenez <rubendariogimenez@gmail.com>
2023-02-08 15:46:41 -0300

    add profile pages add wallet page

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,59 +1,55 @@
-import Header from "../components/Header";
+import TopBar from "../components/TopBar";
 
 const Profile = () => {
     return (
-    <div className="portrait-container">
-        <div className="bg-dark">
-        <div className="topbar__container ml-10">
-                <a className="topbar__back" href="#">
-                    <img className="topbar__back--img" src="/src/assets/svg/header_icon_arrow.svg" alt="back" />
-                </a>
-            </div>
-            <div className="profile__img">
-                <img src="/src/assets/png/messi.png" alt="" />
-                <a href="#" className="profile__img--edit-box">
-                    <img src="/src/assets/svg/profile_icon_edit_picture.svg" alt="" />
-                </a>
-            </div>
-            <div className="profile__name">
-                lionel messi
-                <button>
-                    <img src="/src/assets/svg/profile_icon_edit_name.svg" alt="" />
-                </button>
-            </div>
+        <div className="portrait-container">
+            <div className="bg-dark">
+                <TopBar showBackButton={true} />
+                <div className="profile__img">
+                    <img src="/src/assets/png/messi.png" alt="" />
+                    <a href="#" className="profile__img--edit-box">
+                        <img src="/src/assets/svg/profile_icon_edit_picture.svg" alt="" />
+                    </a>
+                </div>
+                <div className="profile__name">
+                    lionel messi
+                    <button>
+                        <img src="/src/assets/svg/profile_icon_edit_name.svg" alt="" />
+                    </button>
+                </div>
 
-            <button className="profile__wallet-btn ">
-                <img className="profile__wallet-btn--icon" src="/src/assets/png/profile_icon_wallet.png" alt="" />
-                <div className="profile__wallet-btn--title">
-                    My Wallet
-                </div>
-                <div className="profile__wallet-btn--arrow ">
-                    <img src="/src/assets/svg/game_button_icon_down.svg" alt="" />
-                </div>
-            </button>
-            <div className="profile__invite">
-                <h4 className="heading heading__3">invite friends</h4>
-                <p className="profile__invite--p">
-                    obtene 
-                    <img src="/src/assets/svg/icon_coin.svg" alt="" />
-                    <span className="heading heading__3">+20</span>
-                    para vos <br /> y
-                    <img src="/src/assets/svg/icon_coin.svg" alt="" />
-                    <span className="heading heading__3">+20</span>
-                    cada nuevo amigo
-                </p>
-                <button className="profile__invite--btn">
-                    <img src="/src/assets/svg/profile_icon_share.svg" alt="" />
-                    Invite friends
+                <button className="profile__wallet-btn ">
+                    <img className="profile__wallet-btn--icon" src="/src/assets/png/profile_icon_wallet.png" alt="" />
+                    <div className="profile__wallet-btn--title">
+                        My Wallet
+                    </div>
+                    <div className="profile__wallet-btn--arrow ">
+                        <img src="/src/assets/svg/game_button_icon_down.svg" alt="" />
+                    </div>
                 </button>
-                <div className="profile__code-box">
-                    <span className="profile__code-box--legend">Tu codio de referido</span>
-                    <span className="profile__code-box--code">lionel messi</span>
-                    <img src="/src/assets/svg/profile_icon_copy.svg" alt="" />
+                <div className="profile__invite">
+                    <h4 className="heading heading__3">invite friends</h4>
+                    <p className="profile__invite--p">
+                        obtene
+                        <img src="/src/assets/svg/icon_coin.svg" alt="" />
+                        <span className="heading heading__3">+20</span>
+                        para vos <br /> y
+                        <img src="/src/assets/svg/icon_coin.svg" alt="" />
+                        <span className="heading heading__3">+20</span>
+                        cada nuevo amigo
+                    </p>
+                    <button className="profile__invite--btn">
+                        <img src="/src/assets/svg/profile_icon_share.svg" alt="" />
+                        Invite friends
+                    </button>
+                    <div className="profile__code-box">
+                        <span className="profile__code-box--legend">Tu codio de referido</span>
+                        <span className="profile__code-box--code">lionel messi</span>
+                        <img src="/src/assets/svg/profile_icon_copy.svg" alt="" />
+                    </div>
                 </div>
-            </div>            
+            </div>
         </div>
-    </div>
     );
 };
 
--- src/pages/Wallet.tsx
+++ src/pages/Wallet.tsx
@@ -1,210 +1,211 @@
-import Header from "../components/Header";
+import TopBar from "../components/TopBar";
 
 const Wallet = () => {
     return (
-    <div className="portrait-container">
-        <div className="bg-dark">
-            <Header showBackButton={false} />
-            {/* diamond mode wallet */}
-            <div className="wallet-status border-diamond-box">
-                <h4 className="heading heading__1">diamond balance</h4>
-                <div className="wallet-status__balance">
-                    <img src="/src/assets/svg/icon_diamond.svg" alt="" />
-                    <span>450</span>
+        <div className="portrait-container">
+            <div className="bg-dark">
+                <TopBar showBackButton={true} />
+
+                {/* diamond mode wallet */}
+                <div className="wallet-status border-diamond-box">
+                    <h4 className="heading heading__1">diamond balance</h4>
+                    <div className="wallet-status__balance">
+                        <img src="/src/assets/svg/icon_diamond.svg" alt="" />
+                        <span>450</span>
+                    </div>
                 </div>
-            </div>
-            <div className="wallet-payment">
-                <div className="wallet-payment__item">
-                    <button className="bg-circle-viopink">
-                        <img src="/src/assets/svg/wallet_icon_deposit.svg" alt="" />
-                    </button>
-                    <span>deposit</span>
+                <div className="wallet-payment">
+                    <div className="wallet-payment__item">
+                        <button className="bg-circle-viopink">
+                            <img src="/src/assets/svg/wallet_icon_deposit.svg" alt="" />
+                        </button>
+                        <span>deposit</span>
+                    </div>
+                    <div className="wallet-payment__item">
+                        <button className="bg-circle-viopink">
+                            <img src="/src/assets/svg/wallet_icon_buy.svg" alt="" />
+                        </button>
+                        <span>exchange</span>
+                    </div>
                 </div>
-                <div className="wallet-payment__item">
-                    <button className="bg-circle-viopink">
-                        <img src="/src/assets/svg/wallet_icon_buy.svg" alt="" />
-                    </button>
-                    <span>exchange</span>
-                </div>
-            </div>
-            <div className="radio-label">
-                <input type="radio" name="label-option" id="label-option-1" className="radio-option-1" checked />
-                <input type="radio" name="label-option" id="label-option-2" className="radio-option-2" />
-                <input type="radio" name="label-option" id="label-option-3" className="radio-option-3" />
-                <label className="label-option-1" 
+                <div className="radio-label">
+                    <input type="radio" name="label-option" id="label-option-1" className="radio-option-1" checked />
+                    <input type="radio" name="label-option" id="label-option-2" className="radio-option-2" />
+                    <input type="radio" name="label-option" id="label-option-3" className="radio-option-3" />
+                    <label className="label-option-1"
                     // for="label-option-1"
-                >transactions</label>
-                <label className="label-option-2" 
+                    >transactions</label>
+                    <label className="label-option-2"
                     // for="label-option-2"
-                >deposits</label>
-                <label className="label-option-3" 
+                    >deposits</label>
+                    <label className="label-option-3"
                     // for="label-option-3"
-                >withdraws</label>
-            </div>
-            <div className="items-list">
-                <div className="items-list__item">
-                    <div className="items-list__item--img-box">
-                        <img src="/src/assets/svg/wallet_transactions_icon_example.svg" alt="" />
-                    </div>
-                    <span className="items-list__item--text-box">
-                        <h6>
-                            crypto sent
-                        </h6>
-                        <span className="items-list__item--succes">
-                            succesful
+                    >withdraws</label>
+                </div>
+                <div className="items-list">
+                    <div className="items-list__item">
+                        <div className="items-list__item--img-box">
+                            <img src="/src/assets/svg/wallet_transactions_icon_example.svg" alt="" />
+                        </div>
+                        <span className="items-list__item--text-box">
+                            <h6>
+                                crypto sent
+                            </h6>
+                            <span className="items-list__item--succes">
+                                succesful
+                            </span>
+                            <div>
+                                to: 234432432
+                            </div>
+                            <div>
+                                16:00 - 18 dic 2022
+                            </div>
                         </span>
-                        <div>
-                            to: 234432432
-                        </div>
-                        <div>
-                            16:00 - 18 dic 2022
-                        </div>
-                    </span>
-                    <span className="items-list__item--price">
-                        $33,333,333
-                    </span>
-                </div>
-                <div className="items-list__item">
-                    <div className="items-list__item--img-box">
-                        <img src="/src/assets/svg/wallet_transactions_icon_example.svg" alt="" />
-                    </div>
-                    <span className="items-list__item--text-box">
-                        <h6>
-                            crypto sent
-                        </h6>
-                        <span className="items-list__item--failed">
-                            failed
+                        <span className="items-list__item--price">
+                            $33,333,333
                         </span>
-                        <div>
-                            to: 234432432
-                        </div>
-                        <div>
-                            16:00 - 18 dic 2022
-                        </div>
-                    </span>
-                    <span className="items-list__item--price c-red">
-                        -$33,333,333
-                    </span>
-                </div>
-                <div className="items-list__item">
-                    <div className="items-list__item--img-box">
-                        <img src="/src/assets/png/wallet-reward.jpeg" alt="" />
                     </div>
-                    <span className="items-list__item--text-box">
-                        <h6>
-                            registration reward
-                        </h6>
-                        <div>
-                            16:00 - 18 dic 2022
+                    <div className="items-list__item">
+                        <div className="items-list__item--img-box">
+                            <img src="/src/assets/svg/wallet_transactions_icon_example.svg" alt="" />
                         </div>
-                    </span>
-                    <span className="items-list__item--price">
-                        $33,333,333
-                    </span>
+                        <span className="items-list__item--text-box">
+                            <h6>
+                                crypto sent
+                            </h6>
+                            <span className="items-list__item--failed">
+                                failed
+                            </span>
+                            <div>
+                                to: 234432432
+                            </div>
+                            <div>
+                                16:00 - 18 dic 2022
+                            </div>
+                        </span>
+                        <span className="items-list__item--price c-red">
+                            -$33,333,333
+                        </span>
+                    </div>
+                    <div className="items-list__item">
+                        <div className="items-list__item--img-box">
+                            <img src="/src/assets/png/wallet-reward.jpeg" alt="" />
+                        </div>
+                        <span className="items-list__item--text-box">
+                            <h6>
+                                registration reward
+                            </h6>
+                            <div>
+                                16:00 - 18 dic 2022
+                            </div>
+                        </span>
+                        <span className="items-list__item--price">
+                            $33,333,333
+                        </span>
+                    </div>
                 </div>
-            </div>
-            <br />
-            {/* coin mode wallet */}
-            <br />
-            <div className="wallet-status border-coin-box">
-                <h4 className="heading heading__1">coin balance</h4>
-                <div className="wallet-status__balance">
-                    <img src="/src/assets/svg/icon_coin.svg" alt="" />
-                    <span>450</span>
+                <br />
+                {/* coin mode wallet */}
+                <br />
+                <div className="wallet-status border-coin-box">
+                    <h4 className="heading heading__1">coin balance</h4>
+                    <div className="wallet-status__balance">
+                        <img src="/src/assets/svg/icon_coin.svg" alt="" />
+                        <span>450</span>
+                    </div>
                 </div>
-            </div>
-            <div className="wallet-payment">
-                <div className="wallet-payment__item">
-                    <button className="bg-circle-oranyellow">
-                        <img src="/src/assets/svg/wallet_icon_buy.svg" alt="" />
-                    </button>
-                    <span>buy</span>
+                <div className="wallet-payment">
+                    <div className="wallet-payment__item">
+                        <button className="bg-circle-oranyellow">
+                            <img src="/src/assets/svg/wallet_icon_buy.svg" alt="" />
+                        </button>
+                        <span>buy</span>
+                    </div>
+                    <div className="wallet-payment__item">
+                        <button className="bg-circle-oranyellow">
+                            <img src="/src/assets/svg/wallet_icon_withdraw.svg" alt="" />
+                        </button>
+                        <span>withdraw</span>
+                    </div>
                 </div>
-                <div className="wallet-payment__item">
-                    <button className="bg-circle-oranyellow">
-                        <img src="/src/assets/svg/wallet_icon_withdraw.svg" alt="" />
-                    </button>
-                    <span>withdraw</span>
-                </div>
-            </div>
-            <div className="radio-label">
-                <input type="radio" name="label-option" id="label-option-1" className="radio-option-1" checked />
-                <input type="radio" name="label-option" id="label-option-2" className="radio-option-2" />
-                <input type="radio" name="label-option" id="label-option-3" className="radio-option-3" />
-                <label className="label-option-1" 
+                <div className="radio-label">
+                    <input type="radio" name="label-option" id="label-option-1" className="radio-option-1" checked />
+                    <input type="radio" name="label-option" id="label-option-2" className="radio-option-2" />
+                    <input type="radio" name="label-option" id="label-option-3" className="radio-option-3" />
+                    <label className="label-option-1"
                     // for="label-option-1"
-                >transactions</label>
-                <label className="label-option-2" 
+                    >transactions</label>
+                    <label className="label-option-2"
                     // for="label-option-2"
-                >deposits</label>
-                <label className="label-option-3" 
+                    >deposits</label>
+                    <label className="label-option-3"
                     // for="label-option-3"
-                >withdraws</label>
-            </div>
-            <div className="items-list">
-                <div className="items-list__item">
-                    <div className="items-list__item--img-box">
-                        <img src="/src/assets/svg/wallet_transactions_icon_example.svg" alt="" />
-                    </div>
-                    <span className="items-list__item--text-box">
-                        <h6>
-                            crypto sent
-                        </h6>
-                        <span className="items-list__item--succes">
-                            succesful
-                        </span>
-                        <div>
-                            to: 234432432
-                        </div>
-                        <div>
-                            16:00 - 18 dic 2022
-                        </div>
-                    </span>
-                    <span className="items-list__item--price">
-                        $33,333,333
-                    </span>
+                    >withdraws</label>
                 </div>
-                <div className="items-list__item">
-                    <div className="items-list__item--img-box">
-                        <img src="/src/assets/svg/wallet_transactions_icon_example.svg" alt="" />
-                    </div>
-                    <span className="items-list__item--text-box">
-                        <h6>
-                            crypto sent
-                        </h6>
-                        <span className="items-list__item--failed">
-                            failed
+                <div className="items-list">
+                    <div className="items-list__item">
+                        <div className="items-list__item--img-box">
+                            <img src="/src/assets/svg/wallet_transactions_icon_example.svg" alt="" />
+                        </div>
+                        <span className="items-list__item--text-box">
+                            <h6>
+                                crypto sent
+                            </h6>
+                            <span className="items-list__item--succes">
+                                succesful
+                            </span>
+                            <div>
+                                to: 234432432
+                            </div>
+                            <div>
+                                16:00 - 18 dic 2022
+                            </div>
                         </span>
-                        <div>
-                            to: 234432432
-                        </div>
-                        <div>
-                            16:00 - 18 dic 2022
-                        </div>
-                    </span>
-                    <span className="items-list__item--price c-red">
-                        -$33,333,333
-                    </span>
-                </div>
-                <div className="items-list__item">
-                    <div className="items-list__item--img-box">
-                        <img src="/src/assets/png/wallet-reward.jpeg" alt="" />
+                        <span className="items-list__item--price">
+                            $33,333,333
+                        </span>
                     </div>
-                    <span className="items-list__item--text-box">
-                        <h6>
-                            registration reward
-                        </h6>
-                        <div>
-                            16:00 - 18 dic 2022
+                    <div className="items-list__item">
+                        <div className="items-list__item--img-box">
+                            <img src="/src/assets/svg/wallet_transactions_icon_example.svg" alt="" />
                         </div>
-                    </span>
-                    <span className="items-list__item--price">
-                        $33,333,333
-                    </span>
+                        <span className="items-list__item--text-box">
+                            <h6>
+                                crypto sent
+                            </h6>
+                            <span className="items-list__item--failed">
+                                failed
+                            </span>
+                            <div>
+                                to: 234432432
+                            </div>
+                            <div>
+                                16:00 - 18 dic 2022
+                            </div>
+                        </span>
+                        <span className="items-list__item--price c-red">
+                            -$33,333,333
+                        </span>
+                    </div>
+                    <div className="items-list__item">
+                        <div className="items-list__item--img-box">
+                            <img src="/src/assets/png/wallet-reward.jpeg" alt="" />
+                        </div>
+                        <span className="items-list__item--text-box">
+                            <h6>
+                                registration reward
+                            </h6>
+                            <div>
+                                16:00 - 18 dic 2022
+                            </div>
+                        </span>
+                        <span className="items-list__item--price">
+                            $33,333,333
+                        </span>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
     );
 };
 
